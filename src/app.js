1cf750fb428e3689fc65c1955e6cfc1e5209e0e1
Franz Rony Ventocilla Tamara <99495792+Franz04Rony@users.noreply.github.com>
2023-02-22 11:57:33 -0500

    update

--- src/app.js
+++ src/app.js
@@ -27,7 +27,17 @@ app.post('/add', async function(req,res){
     console.log(rows);
     res.redirect('/')
 })
-
+app.get('/delete/:id', async function(req,res){
+    const {id} = req.params;
+    const [rows] = await pool.query('DELETE FROM alumnos WHERE id = ?', [id]);
+    res.redirect('/')
+})
+app.post('/update/:id', async function(req, res){
+    const {id} = req.params;
+    const nuevoAlumno = req.body;
+    const [rows] = await pool.query(`UPDATE alumnos set ? WHERE id = ?`, [nuevoAlumno,id]);
+    res.redirect('/')
+});
 
 app.listen(PORT, ()=>{
     console.log('Servidor en el puerto', PORT );
--- src/public/main.js
+++ src/public/main.js
@@ -1,4 +1,5 @@
-const boton1 = document.getElementById("b1"); 
+const boton1 = document.getElementById("b1");
+const form2 = document.getElementById("form-update");
 const tabla = document.getElementById("tabla")
 document.body.onload = () =>{
     getData();
@@ -7,16 +8,29 @@ document.body.onload = () =>{
         const data = await response.json();
         length = data.length;
         let temp = "";
+        let j = 0;
         if(data){
             for(let i = 0; i < length; i++){
+                j = j+1;
                 temp += "<tr>";
                 temp += "<th>"+ parseInt(i + 1) +"</th>";
                 temp += "<td>"+ data[i].apellidos +"</td>";
                 temp += "<td>"+ data[i].nombres +"</td>";
                 temp += "<td>"+ data[i].sexo +"</td>";
-                temp += "<td>"+ "aa" +"</td>";
+                temp += "<td>"+ `<a href="/delete/${data[i].id}"><button type="button" class="btn btn-danger">Borrar</button> </a>
+
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2" id="b2">
+                Editar</button>
+                
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal3">
+                Ingresar notas</button>  ` +"</td>";
                 temp += "</tr>";
             }
+
+            const botonEditar = document.getElementById("b2")
+            b2.addEventListener('click', (e)=>{
+                form2.action = `/update/${j}`;
+            })
         }
         document.getElementById("tabla").innerHTML = temp;
     }
@@ -43,3 +57,4 @@ boton1.addEventListener('click', (e)=>{
         document.getElementById("tabla").innerHTML = temp;
     }
 });
+
